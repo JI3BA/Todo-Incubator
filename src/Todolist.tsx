2aa94994f7d927a001eb1671ef06a9111926f49c
JI3BA <lyovkin_artyom@mail.ru>
2023-05-25 17:55:28 +0300

    added functional for Todolist, for example: add new note, filter, delete note. And starting work on checkbox component.

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {Button} from "./components/Button";
 
 type TaskType = {
     id: number,
@@ -9,24 +10,61 @@ type TaskType = {
 
 export const Todolist = () => {
     const [data, setData] = useState<TaskType[]>([
-        {id: 0, title: 'What is it', isDone:true},
-        {id: 1, title: 'Dont give that', isDone:false}
+        {id: 0, title: 'What is it', isDone: true},
+        {id: 1, title: 'Dont give that', isDone: false}
     ])
+
+    const [filterData, setFilterData] = useState<TaskType[]>([])
+
+    const [note, setNote] = useState<TaskType>({
+        id: data.length,
+        title: '',
+        isDone: false
+    })
+
+    useEffect(() => {
+        setFilterData(data)
+    }, [data])
+
+    const addNote = () => {
+        setNote({id: data.length + 1, title: '', isDone: false})
+        setData([...data, note])
+    }
+
+    const removeTask = (id: number) => {
+        setData(data.filter((item: TaskType) => id !== item.id))
+    }
+
+    const showAllTask = () => {
+        setFilterData(data)
+    }
+
+    const showActiveTask = () => {
+        setFilterData(data.filter(item => !item.isDone))
+    }
+
+    const showCompletedTask = () => {
+        setFilterData(data.filter(item => item.isDone))
+    }
 
     return(
         <div>
-            <h3>{}</h3>
+            <h3>Notes</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNote({...note, title: event.target.value})} value={note.title}/>
+                <Button onClick={() => addNote()}>+</Button>
             </div>
             <ul>
-                {data.map((item: TaskType) => <li><input type="checkbox" checked={item.isDone}/> <span>{item.title}</span></li>)}
+                {filterData.map((item: TaskType) =>
+                    <li key={item.id}><input type="checkbox" />
+                        <span>{item.title}</span>
+                        <Button onClick={() => removeTask(item.id)}>x</Button>
+                    </li>)}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <Button onClick={() => showAllTask()}>All</Button>
+                <Button onClick={() => showActiveTask()}>Active</Button>
+                <Button onClick={() => showCompletedTask()}>Completed</Button>
             </div>
         </div>
     )
